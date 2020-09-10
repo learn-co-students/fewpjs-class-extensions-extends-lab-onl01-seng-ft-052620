036918c61a7c26e5e24e9c4c018570bf1adaeeac
francepants <franceline90@gmail.com>
2020-09-10 14:29:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+        const reducer = (acc, curr) => acc + curr
+        return this.sides.reduce(reducer)
+    }
+
+}
+
+class Triangle extends Polygon{
+    get isValid() {
+        if (this.countSides === 3) {
+            
+        }
+    }
+}
